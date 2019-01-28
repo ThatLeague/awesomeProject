91ed0fa2b9df7003c2b82191896e4e47fc591f2f
tmli3b3rm4n <tim_lieberman@yahoo.com>
2019-01-27 21:23:19 -0500

    test

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package awesomeProject
 
-import "fmt"
-
-func main () {
-	println(Multiply(10,	20))
-	fmt.Println(Multiply(20, -10));
-	return
-}
-
-
 // Multiply numbers recusively without using Multiply opperator.
 func Multiply(x int, y int) int {
 	// The easiest and also the most important
--- main_test.go
+++ main_test.go
@@ -5,19 +5,21 @@ import (
 "gotest.tools/assert"
 )
 
+// Fixture type data struct.
 type multiplyTest struct{
 	x, y, a int
 }
 
-
+// t testing.T
+// Runs test against the Multiply func
 func TestMultiply(t *testing.T) {
+
 	t.Parallel()
 	var mapD = []multiplyTest{
 		{10, 20, 200},
 		{100, -20, -2000},
 		{-10, -20, 200},
 	}
-
 
 	var val int
 	for i := 0; i < len(mapD); i++ {
@@ -26,5 +28,4 @@ func TestMultiply(t *testing.T) {
 		val = Multiply(x, y)
 		assert.Equal(t,val, mapD[i].a,"Failed on multiply")
 	}
-
 }
